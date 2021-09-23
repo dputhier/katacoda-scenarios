0f1d1bb9559908fb62ce83716ddd77c1377aa7cc
dputhier <puthier@gmail.com>
2021-09-23 17:29:57 +0200

    in p

--- unix-dp/session04/intro-background.sh
+++ unix-dp/session04/intro-background.sh
@@ -47,3 +47,7 @@ cat "second file" > $HOME/test/second_file.txt
 
 ## Starting point...
 cd $HOME
+
+wget -O data.tar.gz https://zenodo.org/record/3997237/files/FAIR_Bioinfo_data.tar.gz?download=1
+tar -xzf data.tar.gz
+rm data.tar.gz
